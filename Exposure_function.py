80c66223a6b72de6f4fb2b18410a6641370d6304
Shelly Bagchi <shelly.bagchi@gmail.com>
2020-04-22 18:38:52 -0400

    Added distance calculations for each point to each source

--- Exposure_function.py
+++ Exposure_function.py
@@ -3,8 +3,10 @@ from matplotlib.path import Path
 from matplotlib.patches import PathPatch
 import matplotlib.pyplot as plt
 import numpy as np
+import itertools
 
-"""The high-level cell was initially designed and constructed
+"""
+The high-level cell was initially designed and constructed
 to accommodate the size and radiological conditions
 associated with a full-sized Westinghouse commercial plant
 fuel assembly. As such, this particular cell is relatively large
@@ -15,8 +17,7 @@ radiological shielding.
 
 The high-level cell has four remote handling stations and
 multiple remote cameras with live video feed viewing monitors.
-A heavy-duty manipulator with a hoist capability of 1,000 pounds
-
+A heavy-duty manipulator with a hoist capability of 1,000 pounds...
 """
 
 def make(xcoord,ycoord,length, width):
@@ -26,35 +27,35 @@ def make(xcoord,ycoord,length, width):
     points = np.array(list(zip(x_1.flatten(),y_1.flatten())))
     vertices = [((xcoord-length/2), (ycoord-width/2)), ((xcoord-length/2), (ycoord+width/2)), ((xcoord+length/2), (ycoord+width/2)), ((xcoord+length/2), (ycoord-width/2)), (0, 0)]
     return points, vertices
-####################ROBOT COORDINATE
+#################### ROBOT COORDINATE
 robot_x_coord=30  #LOCATION OF THE EQUIPMENT ENTRY DOOR WHERE ROBOT WILL START
 robot_y_coord=30  #LOCATION OF THE EQUIPMENT ENTRY DOOR WHERE ROBOT WILL START
 robot_height=6
-robot_bredth=6
-Robot_points,rvertices=make(robot_x_coord,robot_y_coord,robot_bredth,robot_height)
-###########PLOTTING THE ROBOT################
+robot_breadth=6
+Robot_points,rvertices=make(robot_x_coord,robot_y_coord,robot_breadth,robot_height)
+########### PLOTTING THE ROBOT ################
 rcodes = [Path.MOVETO] + [Path.LINETO]*3 + [Path.CLOSEPOLY]
 robot = Path(rvertices,rcodes)
 robotpatch = PathPatch(robot, facecolor='green', edgecolor='green')
-####################PLOTTING SOURCE 1
+#################### PLOTTING SOURCE 1
 circle=Path.circle((366,76),radius=1,readonly=False)
 source1patch=PathPatch(circle, facecolor='red', edgecolor='green')
-####################PLOTTING SOURCE 2
+#################### PLOTTING SOURCE 2
 circle=Path.circle((182,76),radius=1,readonly=False)
 source2patch=PathPatch(circle, facecolor='red', edgecolor='green')
-####################PLOTTING SOURCE 3
+#################### PLOTTING SOURCE 3
 circle=Path.circle((548,76),radius=1,readonly=False)
 source3patch=PathPatch(circle, facecolor='red', edgecolor='green')
-####################PLOTTING DRY STORAGE CASK
+#################### PLOTTING DRY STORAGE CASK
 circle=Path.circle((200,100),radius=20,readonly=False)
 sourcedrycast1=PathPatch(circle, facecolor='red', edgecolor='red')
-######################PLOTTING DRY STORAGE CASK
+#################### PLOTTING DRY STORAGE CASK
 circle=Path.circle((400,100),radius=20,readonly=False)
 sourcedrycast2=PathPatch(circle, facecolor='red', edgecolor='green')
-######################PLOTTING DRY STORAGE CASK
+#################### PLOTTING DRY STORAGE CASK
 circle=Path.circle((600,100),radius=20,readonly=False)
 sourcedrycast3=PathPatch(circle, facecolor='red', edgecolor='green')
-####################TOOL BOX COORDINATE
+#################### TOOL BOX COORDINATE
 TB_x_coord=400  #LOCATION OF TOOL BOX center
 TB_y_coord=40  #LOCATION OF TOOL BOX center
 TB_length= 200
@@ -80,8 +81,31 @@ plt.ylim(0,152)
 plt.show()
 air_density= 1.205e-3 # Dry air near sea level in g cm3
 
-#####################Function for extrapolating attenuation
-#####################coefficients from NIST tables
+##################### Functions for calculating source distances to every point
+#def distance_2(p1,p2):
+#    distance = math.sqrt( ((p1[0]-p2[0])**2)+((p1[1]-p2[1])**2) )
+#    return distance
+Xpts = range(732)
+Ypts = range(152)
+xv,yv = np.meshgrid(Xpts, Ypts, sparse=False, indexing='xy')
+#coords = np.column_stack((xv.ravel(),yv.ravel()))
+#coords = list(itertools.product(x_points, y_points))
+#print(xv)
+d_source1 = np.zeros([732,152])
+d_source2 = np.zeros([732,152])
+d_source3 = np.zeros([732,152])
+for x in Xpts:
+    for y in Ypts:
+        point = np.array([xv[y,x], yv[y,x]])
+        d_source1[x,y] = np.linalg.norm(point-[366,76])
+        d_source2[x,y] = np.linalg.norm(point-[182,76])
+        d_source3[x,y] = np.linalg.norm(point-[548,76])
+#print(d_source1)
+#print(d_source2)
+#print(d_source3)
+
+##################### Function for extrapolating attenuation
+##################### Coefficients from NIST tables
 def extrapolation(isotope):
     y1,y3,x1,x2,x3=isotope[1],isotope[2],isotope[3],isotope[0],isotope[4]
     k=(y3-y1)/(x3-x1)
@@ -89,7 +113,7 @@ def extrapolation(isotope):
     return y2
 
 
-######################Defining the map #########################
+###################### Defining the map #########################
 room_length= 152 # cm
 room_width= 152 # cm
 Source_location=[20,40]
@@ -99,24 +123,26 @@ X,Y = np.meshgrid(x,y, indexing='xy')
 a=np.array(((0,X),(0,Y)))
 R=np.linalg.norm(a) #The 2 norm of each point gives the distance from the center of the room
 points = np.array(list(zip(X.flatten(),Y.flatten())))
+
 #################### GammaEnergy = np.array([0.662]) # MeV, characteristic gamma for Cs 137
 #################### For hot cell there will be multiple isotopes with different characteristic energies
-
 cesium_137 = np.array([0.662,8.04e-2,7.065e-2,0.6,0.8]) #mass interaction coefficient[MeV,mass interactions ( cm2/g),from Faw and Shultis,energis from Faw and Shultis]
 interaction_Coefficient= np.array([extrapolation(cesium_137)]) #cm2/g
 total_miu= interaction_Coefficient*air_density # 1/cm
 Source_Strength = 9.44601235e+17/(((4* np.pi)*(R*R)))# particles/cm2 isotropic source emmiting 1e24 particles at origin
-attinuation_at_R= np.exp(-(total_miu*np.abs(R))) 
+attenuation_at_R= np.exp(-(total_miu*np.abs(R)))
 ResponseFunction=np.array(1.835e-8*cesium_137[0]*interaction_Coefficient) #R/cm2, R=roentgen
-Exposure_rate=ResponseFunction*Source_Strength*attinuation_at_R # R
-##################Westinghhouse High-level Hot Cell
+Exposure_rate=ResponseFunction*Source_Strength*attenuation_at_R # R
 
+
+################## Westinghhouse High-level Hot Cell
 ##print(X,"X")
 ##print(Y,"Y")
-print (R,"R")
+print(R,"R")
 print(Exposure_rate, "Exposure_rate")
-print (points)
-###########PLOTTING THE SOURCET################
+print(points)
+
+########### PLOTTING THE SOURCET################
 circle=Path.circle((75,75),radius=170,readonly=False)
 sourcepatch = PathPatch(circle, facecolor='None', edgecolor='green')
 fig1, ax = plt.subplots()
@@ -129,4 +155,3 @@ ax1.autoscale_view()
 plt.xlim(0,300)
 plt.ylim(0,200)
 plt.show()
-
