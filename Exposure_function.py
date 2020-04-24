f459467946a99fced5870c17cd43c26771feaea8
sargeo80 <60763408+sargeo80@users.noreply.github.com>
2020-04-24 17:30:39 -0400

    fixed some visualization stuff

--- Exposure_function.py
+++ Exposure_function.py
@@ -42,10 +42,10 @@ robotpatch = PathPatch(robot, facecolor='green', edgecolor='green')
 circle=Path.circle((366,76),radius=1,readonly=False)
 source1patch=PathPatch(circle, facecolor='red', edgecolor='green')
 #################### PLOTTING SOURCE 2
-circle=Path.circle((182,76),radius=1,readonly=False)
+circle=Path.circle((150,120),radius=1,readonly=False)
 source2patch=PathPatch(circle, facecolor='red', edgecolor='green')
 #################### PLOTTING SOURCE 3
-circle=Path.circle((548,76),radius=1,readonly=False)
+circle=Path.circle((620,120),radius=1,readonly=False)
 source3patch=PathPatch(circle, facecolor='red', edgecolor='green')
 #################### PLOTTING DRY STORAGE CASK
 circle=Path.circle((200,100),radius=20,readonly=False)
@@ -65,24 +65,24 @@ TB_points, TBvertices=make(TB_x_coord,TB_y_coord,TB_length,TB_height)
 TB = Path(TBvertices,rcodes)
 TBpatch = PathPatch(TB, facecolor='green', edgecolor='green')
 ######PLOTTING#####################
-fig, ax = plt.subplots()
-ax.add_patch(robotpatch)
-ax.add_patch(TBpatch)
-ax.add_patch(source1patch)
-ax.add_patch(source2patch)
-ax.add_patch(source3patch)
-ax.add_patch(sourcedrycast1)
-ax.add_patch(sourcedrycast2)
-ax.add_patch(sourcedrycast3)
-ax.set_title("Westinghouse Hot Cell")
+##fig, ax = plt.subplots()
+##ax.add_patch(robotpatch)
+##ax.add_patch(TBpatch)
+##ax.add_patch(source1patch)
+##ax.add_patch(source2patch)
+##ax.add_patch(source3patch)
+##ax.add_patch(sourcedrycast1)
+##ax.add_patch(sourcedrycast2)
+##ax.add_patch(sourcedrycast3)
+##ax.set_title("Westinghouse Hot Cell")
+##
+##ax.autoscale_view()
+##plt.xlim(0,732)
+##plt.ylim(0,152)
 
-ax.autoscale_view()
-plt.xlim(0,732)
-plt.ylim(0,152)
-##plt.show()
 
 
-##################### Densities ######################################
+####################### Densities ######################################
 argon_density= 0.001784 # Dry air near sea level in g cm3
 lead_density = 11.36
 concrete_density = 3.15
@@ -90,38 +90,32 @@ concrete_density = 3.15
 cesium_137 = np.array([0.662,8.04e-2,7.065e-2,0.6,0.8]) #mass interaction coefficient[MeV,mass interactions ( cm2/g),from Faw and Shultis,energis from Faw and Shultis]
 concrete = np.array([0.662,8.062e-2,6.083e-2, 0.6,0.8])
 lead = np.array([0.662,1.167e-1,8.408e-2, 0.6,0.8])
-
-
-##################### Function for extrapolating attenuation
-##################### Coefficients from NIST tables
+####################### Function for extrapolating attenuation
+####################### Coefficients from NIST tables
 def extrapolation(isotope):
     y1,y3,x1,x2,x3=isotope[1],isotope[2],isotope[3],isotope[0],isotope[4]
     k=(y3-y1)/(x3-x1)
     y2=(k*(x2-x1))+y1
     return y2
-
-
-###################### Defining the map #########################
+#################### Defining the map #########################
 room_length = 732 # cm
 room_width = 152 # cm
-Source1_location = [366,76]
-Source2_location = [182,76]
-Source3_location = [548,76]
+Source1_location = [400,60]
+Source2_location = [150,120]
+Source3_location = [620,120]
 x=np.linspace(-room_length/2,room_length/2,room_length+1)
 y=np.linspace(-room_width/2,room_width/2,room_width+1)
 X,Y = np.meshgrid(x,y, indexing='xy')
 a=np.array(((0,X),(0,Y)))
 R=np.linalg.norm(a) #The 2 norm of each point gives the distance from the center of the room
 points = np.array(list(zip(X.flatten(),Y.flatten())))
-
-
-##################### Calculate source distances to every point
+####################### Calculate source distances to every point
 Xpts = range(room_length)
 Ypts = range(room_width)
 xv,yv = np.meshgrid(Xpts, Ypts, sparse=False, indexing='xy')
-#coords = np.column_stack((xv.ravel(),yv.ravel()))
-#coords = list(itertools.product(x_points, y_points))
-#print(xv)
+coords = np.column_stack((xv.ravel(),yv.ravel()))
+##coords = list(itertools.product(x_points, y_points))
+print(xv)
 d_source1 = np.zeros([room_width, room_length])
 d_source2 = np.zeros([room_width, room_length])
 d_source3 = np.zeros([room_width, room_length])
@@ -139,32 +133,32 @@ for x in Xpts:
         d_cask3[y,x] = np.linalg.norm(point - [600,100])
 ##print(d_source1)
 
-#### ADJUST CASK RADIUS - set to 0 if over 20 (radius of cask)
+###### ADJUST CASK RADIUS - set to 0 if over 20 (radius of cask)
 threshold_indices = d_cask1 > 20
 d_cask1[threshold_indices] = 0
-threshold_indices = d_cask2 > 20
+threshold_indices = d_cask2 > 40
 d_cask2[threshold_indices] = 0
 threshold_indices = d_cask3 > 20
 d_cask3[threshold_indices] = 0
-
-
-#################### GammaEnergy = np.array([0.662]) # MeV, characteristic gamma for Cs 137
-#################### For hot cell there will be multiple isotopes with different characteristic energies
-###################  0.662 Mev Interaction Coefficients
+###################### GammaEnergy = np.array([0.662]) # MeV, characteristic gamma for Cs 137
+###################### For hot cell there will be multiple isotopes with different characteristic energies
+#####################  0.662 Mev Interaction Coefficients
 interaction_Coefficient= np.array([extrapolation(cesium_137)]) #cm2/g
 concrete_interaction_Coefficient= np.array([extrapolation(concrete)]) #cm2/g
 lead_interaction_Coefficient= np.array([extrapolation(lead)]) #cm2/g
-################### Total Mui
+##################### Total Mui
 total_miu= interaction_Coefficient*argon_density # 1/cm
 lead_total_miu= interaction_Coefficient*lead_density # 1/cm
 concrete_total_miu= interaction_Coefficient*concrete_density # 1/cm
-################### Source Strength
-source1_strength= 9.44601235e+17/(((4* np.pi)*(d_source1**2)))
-source2_strength= 9.44601235e+17/(((4* np.pi)*(d_source2**2)))
-source3_strength= 9.44601235e+17/(((4* np.pi)*(d_source3**2)))
+##################### Source Strength
+##
+source1_strength= (9.44601235e+14/3)/(((4* np.pi)*(d_source1**2)))
+source2_strength= (9.44601235e+14/6)/(((4* np.pi)*(d_source2**2)))
+source3_strength= (9.44601235e+14/2)/(((4* np.pi)*(d_source3**2)))
+##
 Source_Strength = 9.44601235e+17/(((4* np.pi)*(R**2)))# particles/cm2 isotropic source emmiting 1e24 particles at origin
-
-################## Attenuations
+##
+#################### Attenuations
 source1attenuation_at_R= np.exp(-(total_miu*np.abs(d_source1)))# ARGON ATTENUATION AT ALL POINTS IN THE ROOM FOR SOURCE 1
 source2attenuation_at_R= np.exp(-(total_miu*np.abs(d_source2)))# ARGON ATTENUATION AT ALL POINTS IN THE ROOM FOR SOURCE 2
 source3attenuation_at_R= np.exp(-(total_miu*np.abs(d_source3)))# ARGON ATTENUATION AT ALL POINTS IN THE ROOM FOR SOURCE 3
@@ -172,21 +166,21 @@ cask1attenuation= np.exp(-(lead_total_miu*np.abs(d_cask1)))# LEAD ATTENUATION FR
 cask2attenuation= np.exp(-(lead_total_miu*np.abs(d_cask2)))# LEAD ATTENUATION FROM CENTER OF CASK 2
 cask3attenuation= np.exp(-(lead_total_miu*np.abs(d_cask3)))# LEAD ATTENUATION FROM CENTER OF CASK 3
 totalcaskattenuation=cask1attenuation+cask2attenuation+cask3attenuation
-
+##
 attenuation_at_R= np.exp(-(total_miu*np.abs(R)))
-
+##
 ResponseFunction=np.array(1.835e-8*cesium_137[0]*interaction_Coefficient) #R/cm2, R=roentgen
 Exposure_rate=ResponseFunction*Source_Strength*attenuation_at_R # R
-##Ereduction=ResponseFunction*Source_Strength*totalcaskattenuation
 Exposure_rate_source1=ResponseFunction*source1_strength*source1attenuation_at_R # R
 Exposure_rate_source2=ResponseFunction*source2_strength*source2attenuation_at_R # R
 Exposure_rate_source3=ResponseFunction*source3_strength*source3attenuation_at_R # R
 Total_Exposure=(Exposure_rate_source1+Exposure_rate_source2+Exposure_rate_source3)*totalcaskattenuation
-print ("\nTotal exposure")
-print (Total_Exposure)
-
-
-################## Exporting total exposure into file
+EX=np.ma.array(Total_Exposure)
+##print ("\nTotal exposure")
+##print (Total_Exposure)
+##
+##
+#################### Exporting total exposure into file
 exposure_dict = {}
 # Discretize every 5cm
 Xgrid = range(0, room_length, 5)
@@ -222,7 +216,19 @@ im = ax2.imshow(Total_Exposure, norm=LogNorm())
 ax2.set_title('Total exposure rate in log scale (scenario 2)')
 cb = fig2.colorbar(im)
 cb.set_label("Radiation exposure rate (R/hr)")
-
+ax2.add_patch(robotpatch)
+ax2.add_patch(TBpatch)
+ax2.add_patch(source1patch)
+ax2.add_patch(source2patch)
+ax2.add_patch(source3patch)
+ax2.add_patch(sourcedrycast1)
+ax2.add_patch(sourcedrycast2)
+ax2.add_patch(sourcedrycast3)
+ax2.autoscale_view()
+plt.xlim(0,732)
+plt.ylim(0,152)
+##plt.plot(ax2)
+##plt.plot(ax)
 ########### PLOTTING THE SOURCE ################
 #fig2, ax2 = plt.subplots()
 #circle=Path.circle((75,75),radius=170,readonly=False)
@@ -232,6 +238,8 @@ cb.set_label("Radiation exposure rate (R/hr)")
 #ax2.autoscale_view()
 #plt.xlim(0,300)
 #plt.ylim(0,200)
-
-
+fig3, ax3 = plt.subplots()
+ax3.plot(EX)
+plt.xlim(0,732)
+plt.ylim(0,152)
 plt.show()
